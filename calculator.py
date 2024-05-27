5b4199d6de76af6600664b35671a1b350283888f
mazzi2023 <mazilugabriel2009@gmail.com>
2024-05-27 14:09:27 +0300

    .

--- calculator.py
+++ calculator.py
@@ -16,12 +16,19 @@ operations = {
     "*": multiply,
     "/": divide
 }
+should_continue = True
 
-num1 = int(input("Please enter your first number:"))
-num2 = int(input("Please enter your seccond number:"))
-for symbol in operations:
-    print(symbol)
-operations_symbol = input("Please choose a symbol from the above list")
-calculation_function = operations[operations_symbol]
-answare = calculation_function(num1, num2)
-print(f"{num1} {operations_symbol} {num2} = {answare}")
+while should_continue:
+    num1 = int(input("Please enter your first number:"))
+    for symbol in operations:
+        print(symbol)
+    operations_symbol = input("Please choose a operation")
+    num2 = int(input("Please enter your seccond number:"))
+    calculation_function = operations[operations_symbol]
+    answare = calculation_function(num1, num2)
+    
+    print(f"{num1} {operations_symbol} {num2} = {answare}")
+    if input("Type 'y' to continue: ") == "y":
+        num1 = answare
+    else:
+        should_continue = False
