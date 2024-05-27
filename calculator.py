96ede3744ee1f40d689dd16d801918ec7a7f2807
mazzi2023 <mazilugabriel2009@gmail.com>
2024-05-27 12:17:48 +0300

    .

--- calculator.py
+++ calculator.py
@@ -1,8 +1,27 @@
-#Functions with Outputs
+from art import logo
+print(logo)
 
-def format_name(f_name, l_name):
-    formated_f_name = f_name.title()
-    formated_l_name = l_name.title()
-    return f"{formated_f_name} {formated_l_name}"
+def add(n1, n2):
+    return n1 + n2
+def substract(n1, n2):
+    return n1 - n2
+def multiply(n1, n2):
+    return n1 * n2
+def divide(n1, n2):
+    return n1 / n2
 
-print(format_name(input("What is your first name?"), input("What is your last name?")))
+operations = {
+    "+": add,
+    "-": substract,
+    "*": multiply,
+    "/": divide
+}
+
+num1 = int(input("Please enter your first number:"))
+num2 = int(input("Please enter your seccond number:"))
+for symbol in operations:
+    print(symbol)
+operations_symbol = input("Please choose a symbol from the above list")
+calculation_function = operations[operations_symbol]
+answare = calculation_function(num1, num2)
+print(f"{num1} {operations_symbol} {num2} = {answare}")
